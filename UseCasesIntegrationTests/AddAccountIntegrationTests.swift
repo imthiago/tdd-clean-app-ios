b132966191d902cbdb5e5514df72e8bb018e147b
Thiago Oliveira <thiago@outlook.com.br>
2020-09-25 01:24:48 -0300

    add failure test case

--- UseCasesIntegrationTests/AddAccountIntegrationTests.swift
+++ UseCasesIntegrationTests/AddAccountIntegrationTests.swift
@@ -11,7 +11,7 @@ import Infra
 import Domain
 
 class AddAccountIntegrationTests: XCTestCase {
-    func test_add_account() {
+    func test_add_account_successfully() {
         let alamofireAdapter = AlamofireAdapter()
         let url = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
         let sut = RemoteAddAccount(url: url, httpClient: alamofireAdapter)
@@ -25,6 +25,24 @@ class AddAccountIntegrationTests: XCTestCase {
                 XCTAssertNotNil(account.id)
                 XCTAssertEqual(account.name, addAccountModel.name)
                 XCTAssertEqual(account.email, addAccountModel.email)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5)
+    }
+    
+    func test_add_account_failure() {
+        let alamofireAdapter = AlamofireAdapter()
+        let url = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
+        let sut = RemoteAddAccount(url: url, httpClient: alamofireAdapter)
+        let addAccountModel = AddAccountModel(name: "Thiago Oliveira", email: "thiago@outlook.com.br", password: "Senha@123", passwordConfirmation: "Senha@1234")
+        let exp = expectation(description: "waiting")
+        sut.add(addAccountModel: addAccountModel) { result in
+            switch result {
+            case .failure(let error):
+                XCTAssertEqual(error, .unexpected)
+            case .success:
+                XCTFail("Expected success got \(result) instead")
             }
             exp.fulfill()
         }
