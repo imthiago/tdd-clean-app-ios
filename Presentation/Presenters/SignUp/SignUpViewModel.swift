4d0730a3a9426f8ad93dd18bd9116a4b4cea1943
Thiago Oliveira <thiago@outlook.com.br>
2020-09-29 23:26:43 -0300

    move SignUpViewModel to its own file

---
+++ Presentation/Presenters/SignUp/SignUpViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpViewModel.swift
+//  Presentation
+//
+//  Created by Thiago Oliveira on 29/09/20.
+//
+
+import Foundation
+
+public struct SignUpViewModel {
+    public var name: String?
+    public var email: String?
+    public var password: String?
+    public var passwordConfirmation: String?
+    
+    public init(name: String? = nil, email: String? = nil, password: String? = nil, passwordConfirmation: String? = nil) {
+        self.name = name
+        self.email = email
+        self.password = password
+        self.passwordConfirmation = passwordConfirmation
+    }
+}
