27c652c9653678d2f73eec58e18615cf7fa7a5c3
Thiago Oliveira <thiago@outlook.com.br>
2020-09-30 00:11:11 -0300

    adding UI layer

---
+++ UITests/SignUpViewControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by Thiago Oliveira on 30/09/20.
+//
+
+import XCTest
+
+class SignUpViewControllerTests: XCTestCase {
+    func test_() {
+        
+    }
+}
