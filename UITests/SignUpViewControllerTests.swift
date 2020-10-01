b036fbfb0d73926648ee4e1d9b3523204951d12a
Thiago Oliveira <thiago@outlook.com.br>
2020-09-30 23:35:04 -0300

    ensure SignUpViewController implements AlertView

--- UITests/SignUpViewControllerTests.swift
+++ UITests/SignUpViewControllerTests.swift
@@ -18,9 +18,15 @@ class SignUpViewControllerTests: XCTestCase {
         XCTAssertEqual(sut.loadingIndicator?.isAnimating, false)
     }
     
-    func test_sut_implements_isLoadingView() {
+    func test_sut_implements_loadingView() {
         let sb = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
         let sut = sb.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
         XCTAssertNotNil(sut as LoadingView)
+    }
+    
+    func test_sut_implements_alertView() {
+        let sb = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
+        let sut = sb.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
+        XCTAssertNotNil(sut as AlertView)
     }
 }
