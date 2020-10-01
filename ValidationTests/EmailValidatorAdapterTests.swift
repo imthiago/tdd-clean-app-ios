20f52626d70727670f156f2e79216099e8f4e8ff
Thiago Oliveira <thiago@outlook.com.br>
2020-10-01 19:01:29 -0300

    add validation target

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,14 @@
+//
+//  ValidationTests.swift
+//  ValidationTests
+//
+//  Created by Thiago Oliveira on 01/10/20.
+//
+
+import XCTest
+
+class EmailValidatorAdapterTests: XCTestCase {
+    func test_()  {
+        
+    }
+}
