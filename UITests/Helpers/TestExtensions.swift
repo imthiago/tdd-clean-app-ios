2aa1c150e309798fa8ab96ee7dd58a3f56a5576f
Thiago Oliveira <thiago@outlook.com.br>
2020-10-01 00:19:42 -0300

    move UiExtension to a file

---
+++ UITests/Helpers/TestExtensions.swift
@@ -0,0 +1,23 @@
+//
+//  TestExtensions.swift
+//  UITests
+//
+//  Created by Thiago Oliveira on 01/10/20.
+//
+
+import Foundation
+import UIKit
+
+extension UIControl {
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach({ action in
+                (target as NSObject).perform(Selector(action))
+            })
+        }
+    }
+    
+    func simulateTap() {
+        simulate(event: .touchUpInside)
+    }
+}
--- UITests/Scenes/SignUp/SignUpViewControllerTests.swift
+++ UITests/Scenes/SignUp/SignUpViewControllerTests.swift
@@ -48,16 +48,3 @@ extension SignUpViewControllerTests {
     }
 }
 
-extension UIControl {
-    func simulate(event: UIControl.Event) {
-        allTargets.forEach { target in
-            actions(forTarget: target, forControlEvent: event)?.forEach({ action in
-                (target as NSObject).perform(Selector(action))
-            })
-        }
-    }
-    
-    func simulateTap() {
-        simulate(event: .touchUpInside)
-    }
-}
