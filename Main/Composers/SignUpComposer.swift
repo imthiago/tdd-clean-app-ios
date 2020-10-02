ab32455caa35bde419d7ab3da44f41526b1594ed
Thiago Oliveira <thiago@outlook.com.br>
2020-10-01 23:37:32 -0300

    improve code readability

--- Main/Composers/SignUpComposer.swift
+++ Main/Composers/SignUpComposer.swift
@@ -6,12 +6,20 @@
 //
 
 import Foundation
-import Domain
 import UI
+import Presentation
+import Validation
+import Data
+import Infra
+import Domain
 
 public final class SignUpComposer {
     public static func composeControllerWith(addAccount: AddAccount) -> SignUpViewController {
-        return ControllerFactory.makeSignUp(addAccount: addAccount)
+        let controller = SignUpViewController.instanciate()
+        let emailValidatorAdapter = EmailValidatorAdapter()
+        let presenter = SignUpPresenter(alertView: WearkVarProxy(controller), emailValidator: emailValidatorAdapter, addAccount: addAccount, loadingView: WearkVarProxy(controller))
+        controller.signUp = presenter.signUp
+        return controller
     }
 }
 
